d7389d3d7f84d6f6e564a2e1fcf044b01d450c7c
Adel Khedhiri <khedhiri.adel@gmail.com>
2024-08-15 17:45:09 +0100

    7 commit

--- user.py
+++ user.py
@@ -23,3 +23,21 @@ class User:
     def save(cls,data):
         query= "INSERT INTO users (first_name,last_name,email) VALUES (%(first_name)s,%(last_name)s,%(email)s);"
         return connectToMySQL(DB).query_db(query,data)
+    
+    @classmethod
+    def update_user(cls, data):
+            query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+            return connectToMySQL(DB).query_db(query, data)
+       
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL(DB).query_db(query, data)
+        if result:
+            return cls(result[0])
+        return None
+    @classmethod
+    def delete(cls, user_id):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        data = {"id": user_id}
+        return connectToMySQL(DB).query_db(query, data)
