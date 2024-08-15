d2006c8fca4447e155f69933e943bbcfaa00b22f
Adel Khedhiri <khedhiri.adel@gmail.com>
2024-08-15 12:38:53 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask , render_template, request, redirect, url_for
+from user import User
+app = Flask(__name__)
+
+
+
+@app.route("/user/new")
+def new_user():
+    return render_template("new_user.html")
+
+@app.route("/user/new", methods=['POST'])
+def create_user():
+       data = {
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"]
+        }
+ 
+       User.save(data)
+   
+       return redirect(url_for('get_users'))
+
+@app.route("/users")
+def get_users():
+     users= User.get_all()
+     print(users)
+     return render_template('all_users.html',users=users)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
